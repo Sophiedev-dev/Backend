3477d70446ed1c1ef5bb55bc1ae5801208357b88
Sophia Mba <sophiamba17@gmail.com>
2025-05-05 12:52:37 +0100

    PDF reader

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Accept']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
